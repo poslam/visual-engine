f54769e091e825921b833a9a4753ab6c605acc07
deniskirienko <kirden04@gmail.com>
2023-03-14 19:08:56 +1000

    upd

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-import sys
+import globals 
 
 class Point:
     def __init__(self, c1: float, c2: float, c3: float):
@@ -9,9 +9,9 @@ class Point:
     def as_list(self):
         return [self.c1, self.c2, self.c3]
     
-    def distance(point1: __init__, point2: __init__):
-        return ((point1.c1 - point2.c1)**2 + (point1.c2 - point2.c2)**2 
-                + (point1.c3 - point2.c3)**2)**0.5
+    def distance(self, point: __init__):
+        return ((self.c1 - point.c1)**2 + (self.c2 - point.c2)**2 
+                + (self.c3 - point.c3)**2)**0.5
             
     def __add__(self, another_point: __init__):
         return Point(
@@ -20,36 +20,42 @@ class Point:
             c3 = self.c3 + another_point.c3
         )
         
-    def __mul__(self, scalar: float):
+    def __mul__(self, operand):
         return Point(
-            c1 = self.c1 * scalar,
-            c2 = self.c2 * scalar,
-            c3 = self.c3 * scalar
+            c1 = self.c1 * operand,
+            c2 = self.c2 * operand,                
+            c3 = self.c3 * operand
         )
+    
+    def __rmul__(self, operand):
+        return self.__mul__(operand)
         
-    def __sub__(self, another_point: __init__):
+    def __sub__(self, another_point: 'Point'):
         return self+(another_point*(-1))
     
     def __truediv__(self, scalar: float):
         if scalar == 0:
-            print({"msg": "division by zero"})
-            sys.exit()
+            raise Exception(ZeroDivisionError)
+        
         return Point(
             c1 = self.c1 / scalar,
             c2 = self.c2 / scalar,
             c3 = self.c3 / scalar
         )
+        
+    def __repr__(self):
+        return f"Point({self.c1}, {self.c2}, {self.c3})"
             
           
 class Vector:
-    def __init__(self, c1: float = None,
+    def __init__(self, c1: [float, Point] = None,
                  c2: float = None, c3: float = None):
-        if type(c1) == int or type(c1) == float:
+        if isinstance(c1, (int, float)):
             self.c1 = c1
             self.c2 = c2
             self.c3 = c3
-        
-        else:    
+
+        elif isinstance(c1, Point):   
             self.c1 = c1.c1
             self.c2 = c1.c2
             self.c3 = c1.c3
@@ -60,8 +66,8 @@ class Vector:
     def as_point(self):
         return Point(c1 = self.c1, c2 = self.c2, c3 = self.c3)
     
-    def len(self, base):
-        return Point.distance(base.initial_point, self)
+    def len(self):
+        return Point.distance(globals.VSpace.initial_point, self)
     
     def __add__(self, another_vec: __init__):
         return Vector(
@@ -69,77 +75,72 @@ class Vector:
             c2 = self.c2 + another_vec.c2,
             c3 = self.c3 + another_vec.c3
         )
+        
+    def scalar_product(self, vector: 'Vector') -> [int, float]:
+        return self.c1 * vector.c1 + self.c2 * vector.c2 + self.c3 * vector.c3
     
-    def __mul__(self, scalar_or_vector: __init__):
-        
-        if type(scalar_or_vector) == int or type(scalar_or_vector) == float:
-            return Vector(
-                c1 = self.c1 * scalar_or_vector,
-                c2 = self.c2 * scalar_or_vector,
-                c3 = self.c3 * scalar_or_vector
-            )
-            
-        scalar_or_vector = scalar_or_vector.as_list()
-        another_vector = Vector(c1 = scalar_or_vector[0],
-                                c2 = scalar_or_vector[1],
-                                c3 = scalar_or_vector[2])
-        return (self.c1 * another_vector.c1 + self.c2 * another_vector.c2 + 
-            self.c3 * another_vector.c3)
-        
-    def __pow__(self, another_vector: __init__):
+    def vector_product(self, vector: 'Vector'):
         return Vector(
-            self.c2 * another_vector.c3 - self.c3 * another_vector.c2,
-            self.c3 * another_vector.c1 - self.c1 * another_vector.c3,
-            self.c1 * another_vector.c2 - self.c2 * another_vector.c1
+            self.c2 * vector.c3 - self.c3 * vector.c2,
+            self.c3 * vector.c1 - self.c1 * vector.c3,
+            self.c1 * vector.c2 - self.c2 * vector.c1
         )
+    
+    def __mul__(self, object: [int, float, 'Vector']):
         
-    def mixed_mul(vec1: __init__, vec2: __init__, vec3: __init__):
-        return vec1 * (vec2 ** vec3)
-        
-    def norm(self, base):
-        len = self.len(base)
-        if len != 0:
+        if isinstance(object, (int, float)):
             return Vector(
-                c1= self.c1/len, c2=self.c2/len, c3=self.c3/len
+                c1 = self.c1 * object,
+                c2 = self.c2 * object,
+                c3 = self.c3 * object
             )
-        print({"msg": "division by zero"})
-        sys.exit()
+        
+        if isinstance(object, Vector):
+            return self.scalar_product(object)
+        
+    def __rmul__(self, object: [int, float, 'Vector']):
+        return self.__mul__(object)
+    
+    def __truediv__(self, scalar: float):
+        if scalar == 0:
+            raise Exception(ZeroDivisionError)
+        
+        return self*(1/scalar)
+
+    def __pow__(self, object: 'Vector'):
+        if isinstance(object, Vector):
+            return self.vector_product(object)
+        
+    # @staticmethod
+    # def mixed_mul(vec1: __init__, vec2: __init__, vec3: __init__):
+    #     return vec1 * (vec2 ** vec3)
+        
+    def norm(self):
+        return self*(1/self.len())
+        
+    def __repr__(self):
+        return f"Vector({self.c1}, {self.c2}, {self.c3})"
         
         
 class VectorSpace:
+    initial_point = Point(0, 0, 0)
+    
     def __init__(self, initial_point: Point,
                  e1: Vector, e2: Vector, e3: Vector):
         self.initial_point = initial_point
-        self.e1 = self.norm(e1)
-        self.e2 = self.norm(e2)
-        self.e3 = self.norm(e3)
-        # self.e1 = e1
-        # self.e2 = e2
-        # self.e3 = e3
-        
-    def len(self, point2: Point):
-        return Point.distance(self.initial_point, point2)
-    
-    def norm(self, vec: Vector):
-        len = self.len(vec.as_point())
-        if len != 0:
-            return Vector(
-                c1=vec.c1/len, c2=vec.c2/len, c3=vec.c3/len
-            )
-        print({"msg": "division by zero"})
-        sys.exit()
-        
+        self.e1 = e1.norm()
+        self.e2 = e2.norm()
+        self.e3 = e3.norm()
 
 class Camera:
-    def __init__(self, position: Point, look_dir: Vector, 
-                 look_at: Point, fov: float, k: float,
+    def __init__(self, position: Point, look_dir_at: [Vector, Point], 
+                 fov: float, k: float,
                  draw_distance: float):
         self.position = position
-        self.look_dir = look_dir
-        self.look_at = look_at
+        self.look_dir_at = look_dir_at
         self.fov = fov
         self.vfov = 2/3*fov*k
-        # k - коэфициент сжатия
+        # k - коэффициент сжатия
         self.draw_distance = draw_distance
         
     def send_rays(self, amount_rays: int):
@@ -147,13 +148,15 @@ class Camera:
     
     
 class Object:
-    def __init__(self, position: Point, rotation: Vector,
-                 obj_points: list):
+    def __init__(self, position: Point, rotation: Vector):
         self.position = position
         self.rotation = position
-        self.obj_points = obj_points
         
     def contains(self, point: Point):
-        return point in self.obj_points
+        return False
     
+# class Ball(Object):
+    
+    # def contains(self, point: Point):
+    #     return self.parameters.center.distance(point) <= self.parameters.radius
     
---
+++ globals.py
@@ -0,0 +1,3 @@
+def Iitialize():
+    global VSpace
+    VSpace = None
