ccb19a060a5ac1cfaa2e29415a140459abd98b2b
deniskirienko <kirden04@gmail.com>
2023-04-10 15:19:35 +1000

    upd

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -11,25 +11,15 @@ from src.classes import *
 m1 = Matrix([[1, 2], [3, 4]])
 m2 = Matrix([[2, 3], [1, 0]])
 m3 = Matrix([[1, 2, 3], [2, 3, 1], [5, 1, 0]])
-m4 = Matrix([[1+10**(-5), 2, 3], [2, 3, 1], [5, 1, 0]])
-m5 = Matrix([[1+10**(-8), 2, 3], [2, 3, 1], [5, 1, 0]])
-m6 = Matrix([[1, 2], [3, 4], [5, 6]])
-m7 = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-m8 = Matrix([[2, 2, 3], [2, 4, 1], [5, 1, 1]])
 
 v1 = Vector([1, 2, 3])
-v2 = Vector([5, 6, 0])
+# v2 = Vector([5, 6, 0])
 v3 = Vector([[2], [3], [4]])
-v4 = Vector([[0], [0], [1]])
+# v4 = Vector([[0], [0], [1]])
 
 vs = VectorSpace([Vector([1, 0, 0]), Vector([0, 1, 0]), Vector([0, 0, 1])])
 
 p1 = Point([1, 1, 1])
-
-zero = Matrix.zero_matrix(2, 5)
-
-i2 = Matrix.identity_matrix(2)
-i3 = Matrix.identity_matrix(3)
 
 
 class TestMatrix:
@@ -44,39 +34,49 @@ class TestMatrix:
         assert isinstance(m1.data, list)
 
     def testSizeAutoComplete(self):
-        assert m6.rows == 3 and m6.columns == 2
+        m = Matrix([[1, 2], [3, 4], [5, 6]])
+        assert m.rows == 3 and m.columns == 2
 
     def testZeroMatrixSize(self):
+        zero = Matrix.zero_matrix(2, 5)
         assert zero.rows == 2 and zero.columns == 5
 
     def testZeroMatrixData(self):
+        zero = Matrix.zero_matrix(2, 5)
         assert all(zero[i][j] == 0 
                    for i in range(2)
                    for j in range(5))
 
     def testIdentityMatrixByProduct(self):
-        assert m1*i2 == i2*m1 == m1
+        i = Matrix.identity_matrix(2)
+        assert m1*i == i*m1 == m1
 
     def testTransposeByDoubleTranspose(self):
         assert m1.transpose().transpose() == m1
 
     def testEquationBySameMatrices(self):
-        assert m3 == m5
+        m = Matrix([[1+10**(-8), 2, 3], [2, 3, 1], [5, 1, 0]])
+        assert m3 == m
 
     def testEpsilonParamInEquation(self):
-        assert m3 != m4
+        m = Matrix([[1+10**(-5), 2, 3], [2, 3, 1], [5, 1, 0]])
+        assert m3 != m
 
     def testEquationByDifferentMatrices(self):
         assert m1 != m2
 
     def testIdentityMatrix(self):
+        m7 = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        i3 = Matrix.identity_matrix(3)
         assert i3 == m7
         
     def testAdditionCorrect(self):
+        m7 = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        m8 = Matrix([[2, 2, 3], [2, 4, 1], [5, 1, 1]])
         assert m3 + m7 == m8
 
     def testAdditionOutType(self):
-        assert isinstance(m3+m7, Matrix)
+        assert isinstance(m1+m2, Matrix)
     
     def testCommutativeOfAddition(self):
         assert m1+m2 == m2+m1
@@ -100,4 +100,25 @@ class TestMatrix:
         with pytest.raises(EngineException):
             m1*"str"
             
-    
+    def testProductOutType(self):
+        assert isinstance(m1*m2, Matrix)
+        
+    def testProductWithScalarOutType(self):
+        assert isinstance(m1*4, Matrix)        
+            
+    def testProductMatrices(self):
+        m6 = Matrix([[1, 2], [3, 4], [5, 6]])
+        m9 = Matrix([[23, 30], [19, 26], [13, 20]])
+        m8 = Matrix([[2, 2, 3], [2, 4, 1], [5, 1, 1]])
+        assert m8*m6 == m9
+        
+    def testProductWithScalar(self):
+        m10 = Matrix([[4, 8, 12], [8, 12, 4], [20, 4, 0]])
+        assert m3*4 == m10
+        
+    def testProductWithScalarCommutative(self):
+        assert m3*4 == 4*m3
+        
+    def testSubtraction(self):
+        m3 = Matrix([[-1, -1], [2, 4]])
+        assert m1-m2 == m3
