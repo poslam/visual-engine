35a62473da6d5e504f363b6b3369957a2c2517ae
deniskirienko <kirden04@gmail.com>
2023-06-06 11:28:32 +1000

    fix game_test

--- test/game_test.py
+++ test/game_test.py
@@ -13,21 +13,26 @@ cs = CoordinateSystem(p1, vs)
 
 globals.cs = cs
 
-g = Game(cs, EntityList(Entity))
-
 class TestIntersection:
     def test_intersection(self):
+        g = Game(cs, EntityList(Entity))
 
         camera = g.camera(Point([0, 0, 0]), draw_distance=100, fov=100,
-                                                direction=Vector([1, 1, 1]))
+                                                direction=Vector([200, 20, 1]))
 
         a = camera.get_rays_matrix(10, 10)
 
-        myg = MyGame(cs, EntityList(Entity))
+        x = Entity(cs)
+        x.pravo = "slavno"
 
-        obj = myg.get_hyperellipsoid()(position=Point([-10, 10, 10]), 
+        myg = MyGame(cs)
+
+        canv = myg.get_canvas()(10, 10)
+
+
+        obj = myg.get_hyperellipsoid()(position=Point([1000, 200, 200]), 
                                     direction=Vector([2, 1, 89]), 
-                                    semiaxes=[0.1, 0.2, 0.5])
+                                    semiaxes=[100, 200, 50])
 
         act = True
         for i in a:
